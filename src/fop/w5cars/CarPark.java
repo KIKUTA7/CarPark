d00731f1f09ae9bcb4d1d02f0f4b0c846303e486
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-03 10:51:31 +0400

    4

--- src/fop/w5cars/CarPark.java
+++ src/fop/w5cars/CarPark.java
@@ -9,13 +9,13 @@ public class CarPark {
     public int park (Car c)
     {
       for (int i=0;i<spaces.length;i++)
-          if(spaces [i] == null) {spaces[i] = c;return i+1;}
+          if(spaces [i] == null) {spaces[i] = c;return i;}
       return -1;
     }
     public int search (LicensePlate lp)
     {
         for (int i=0;i< spaces.length;i++)
-            if(spaces[i].getLicensePlate() == lp) return i+1;
+            if(spaces[i].getLicensePlate() == lp) return i;
             return -1;
     }
 }
