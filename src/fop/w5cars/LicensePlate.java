fefa43670f34e26736c8e344db017a6136a32551
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-03 10:20:09 +0400

    1

--- src/fop/w5cars/LicensePlate.java
+++ src/fop/w5cars/LicensePlate.java
@@ -1,5 +1,13 @@
 package fop.w5cars;
 
 public class LicensePlate {
-	// TODO
+	public String regionalCode , letters;
+	int digits;
+	public LicensePlate (String x , String y , int k)
+    {
+        this.regionalCode = x;
+        this.letters = y;
+        this.digits = k;
+    }
+
 }
