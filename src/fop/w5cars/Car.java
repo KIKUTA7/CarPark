2cfe76269425bd3e940cac70084fdc7dc656a37b
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-03 10:27:29 +0400

    2

--- src/fop/w5cars/Car.java
+++ src/fop/w5cars/Car.java
@@ -3,9 +3,11 @@ package fop.w5cars;
 public class Car {
 	private String brand;
 	private LicensePlate licensePlate;
+	private static int chassisnumber;
 	public Car (String x , LicensePlate y)
     {
         this.brand = x;
         this.licensePlate = y;
+        this.chassisnumber -- ;
     }
 }
