5f1fbb255dafccd86c14b98c250f17ca4ac10529
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-03 10:39:39 +0400

    3

--- src/fop/w5cars/Car.java
+++ src/fop/w5cars/Car.java
@@ -3,7 +3,7 @@ package fop.w5cars;
 public class Car {
 	private String brand;
 	private LicensePlate licensePlate;
-	private static int chassisNumber = 0;
+	public static int chassisNumber = 0;
 	public Car (String x , LicensePlate y)
     {
         this.brand = x;
--- src/fop/w5cars/CarPark.java
+++ src/fop/w5cars/CarPark.java
@@ -1,5 +1,14 @@
 package fop.w5cars;
 
 public class CarPark {
-	// TODO
+	private int spaces;
+	public CarPark(int x)
+    {
+        this.spaces = x;
+    }
+    public int park (Car c)
+    {
+        if (spaces > 0) return --spaces;
+        return -1;
+    }
 }
